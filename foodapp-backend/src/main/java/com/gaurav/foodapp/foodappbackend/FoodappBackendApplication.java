ea7c022f0d3cc08e8021087e614ce1388557580b
Gaurav Sood <g.sood@uqconnect.edu.au>
2020-07-15 15:02:16 +1000

    first Commit :)

---
+++ foodapp-backend/src/main/java/com/gaurav/foodapp/foodappbackend/FoodappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.foodapp.foodappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodappBackendApplication.class, args);
+	}
+
+}
---
+++ foodapp-backend/src/test/java/com/gaurav/foodapp/foodappbackend/FoodappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.foodapp.foodappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
