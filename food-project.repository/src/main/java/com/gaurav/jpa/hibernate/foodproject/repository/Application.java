ff4cc14099b07b00d8ecb58cc762032e1482ae96
gauravuq <g.sood@uq.net.au>
2020-11-25 06:08:03 +1000

    Added Eureka service and spring gateway

--- food-project.repository/src/main/java/com/gaurav/jpa/hibernate/foodproject/repository/Application.java
+++ food-project.repository/src/main/java/com/gaurav/jpa/hibernate/foodproject/repository/Application.java
@@ -12,11 +12,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application  {
 
 	public static void main(String[] args) {
